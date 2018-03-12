c7370af4df3c90add1175457595f90ef9a9a4d07
Unknown <juan_jop96@hotmail.com>
2018-03-12 12:52:56 -0600

    Muestra las universidades para editar

--- TareaMean/src/app/app-routing.module.ts
+++ TareaMean/src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { EscuelaComponent } from './escuela/escuela.component';
-//import { UniversidadComponent } from "./universidad/universidad.component";
+import { UniversidadComponent } from "./universidad/universidad.component";
 
 const routes: Routes = [
 
   {path: '', redirectTo:'/home', pathMatch:'full'},
   {path: 'home', component: HomeComponent},
   {path: 'escuela', component: EscuelaComponent},
-  //{path: 'universidades', component: UniversidadComponent}
+  {path: 'universidades', component: UniversidadComponent}
 ];
 
 @NgModule({
