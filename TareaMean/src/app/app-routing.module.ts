33c10e32eeb60c687e94e6677a5529f781b159a7
Unknown <juan_jop96@hotmail.com>
2018-03-12 23:07:19 -0600

    merge fixed

--- TareaMean/src/app/app-routing.module.ts
+++ TareaMean/src/app/app-routing.module.ts
@@ -3,25 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { EscuelaComponent } from './escuela/escuela.component';
 import { UniversidadComponent } from "./universidad/universidad.component";
-<<<<<<< HEAD
-=======
 import { MateriaComponent } from "./materia/materia.component";
-
->>>>>>> 7fa16a12361133e9f88f1b6eaa6b1022aca2d1d4
 
 const routes: Routes = [
 
   {path: '', redirectTo:'/home', pathMatch:'full'},
   {path: 'home', component: HomeComponent},
-<<<<<<< HEAD
+
   {path: 'escuelas', component: EscuelaComponent},
-  {path: 'universidades', component: UniversidadComponent}
-=======
+  {path: 'universidades', component: UniversidadComponent},
   {path: 'escuela', component: EscuelaComponent},
   {path: 'universidades', component: UniversidadComponent},
   {path: 'materias', component: MateriaComponent},
 
->>>>>>> 7fa16a12361133e9f88f1b6eaa6b1022aca2d1d4
 ];
 
 @NgModule({
--- TareaMean/src/app/app.module.ts
+++ TareaMean/src/app/app.module.ts
@@ -11,16 +11,15 @@ import { HomeComponent } from './home/home.component';
 import { UniversidadComponent } from './universidad/universidad.component';
 import { UniversidadListComponent } from './universidad-list/universidad-list.component';
 import { UniversidadDetalleComponent } from './universidad-detalle/universidad-detalle.component';
-<<<<<<< HEAD
+
 import { EscuelaComponent } from './escuela/escuela.component';
 import { EscuelaListComponent } from './escuela-list/escuela-list.component';
 import { EscuelaDetalleComponent } from './escuela-detalle/escuela-detalle.component';
-=======
-//import { EscuelaComponent } from './escuela/escuela.component';
+
 import { MateriaComponent } from './materia/materia.component';
 import { MateriaDetalleComponent } from './materia-detalle/materia-detalle.component';
 import { MateriaListComponent } from './materia-list/materia-list.component';
->>>>>>> 7fa16a12361133e9f88f1b6eaa6b1022aca2d1d4
+
 
 
 @NgModule({
@@ -30,16 +29,15 @@ import { MateriaListComponent } from './materia-list/materia-list.component';
     UniversidadComponent,
     UniversidadListComponent,
     UniversidadDetalleComponent,
-<<<<<<< HEAD
+
     EscuelaComponent,
     EscuelaListComponent,
-    EscuelaDetalleComponent
-=======
+    EscuelaDetalleComponent,
+
     //EscuelaComponent,
     MateriaComponent,
     MateriaDetalleComponent,
     MateriaListComponent
->>>>>>> 7fa16a12361133e9f88f1b6eaa6b1022aca2d1d4
   ],
   imports: [
     BrowserModule,
